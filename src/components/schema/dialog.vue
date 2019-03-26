09f97b5c188d97296b2cd1a5bea67a62e7e52d71
shadow <wuh131420@foxmail.com>
2019-03-26 18:02:56 +0800

    2019-03-26-[fixed][修复一下dialog的before-close事件]

--- src/components/schema/dialog.vue
+++ src/components/schema/dialog.vue
@@ -2,65 +2,66 @@
   <section class="my-page-dialog">
     <el-dialog
       :visible.sync="isShowDialog"
-      :title="dialogTitle">
-    <el-form
-      label-width="120px"
-      :model="getFormModel.form"
-      ref="myDialogForm" >
-        <el-form-item
-          v-for="(item, index) in getFormModel.list"
-          :key="index"
-          :prop="item.field"
-          :label="item.label"
-          :rule="item.rule">
-          <template v-if="item.type == 'default'">
-            <span>{{getFormModel.form[item.field]}}</span>
-          </template>
-          <template v-if="item.type == 'input'">
-            <el-input
-              v-model="getFormModel.form[item.field]"
-              :placeholder="'请编辑' + item.label"
-              class="my-input__220"/>
-          </template>
-          <template v-if="item.type == 'textarea'">
-            <el-input
-              v-model="getFormModel.form[item.field]"
-              :placeholder="'请编辑' + item.label"
-              type="textarea"
-              :rows="item.rows || 3"
-              class="my-input__220"/>
-          </template>
-          <template v-if="item.type == 'switch'">
-            <el-switch
-              :value="getFormModel.form[item.field]"
-              :active-text="item.activeText"
-              :inactive-text="item.inactiveText"
-              :active-value="item.activeValue"
-              :inactive-value="item.inactiveValue">
-            </el-switch>
-          </template>
-          <template v-if="item.type === 'uploadImg'">
-            <el-upload
-              :action="uploadPath"
-              class="avatar-uploader"
-              :show-file-list="false"
-              name="multipartFile"
-              :data = "{}"
-              :on-success="e => {return hanleUploadSuccess(e, item)}"
-              >
-                <img v-if="getFormModel.form[item.field]"
-                  :src="getFormModel.form[item.field]"
-                  style="width: 175px; height: 175px;"
-                  alt="uploader">
-                <i v-else class="el-icon-plus avatar-uploader-icon" ></i>
-            </el-upload>
-          </template>
-        </el-form-item>
-    </el-form>
-    <div slot="footer">
-      <el-button @click="handleCancel" type="danger">cancel</el-button>
-      <el-button @click="handleSubmit" type="primary">submit</el-button>
-    </div>
+      :title="dialogTitle"
+      :before-close="DIALOG_CLAOSE_SYNC">
+      <el-form
+        label-width="120px"
+        :model="getFormModel.form"
+        ref="myDialogForm">
+          <el-form-item
+            v-for="(item, index) in getFormModel.list"
+            :key="index"
+            :prop="item.field"
+            :label="item.label"
+            :rule="item.rule">
+            <template v-if="item.type == 'default'">
+              <span>{{getFormModel.form[item.field]}}</span>
+            </template>
+            <template v-if="item.type == 'input'">
+              <el-input
+                v-model="getFormModel.form[item.field]"
+                :placeholder="'请编辑' + item.label"
+                class="my-input__220"/>
+            </template>
+            <template v-if="item.type == 'textarea'">
+              <el-input
+                v-model="getFormModel.form[item.field]"
+                :placeholder="'请编辑' + item.label"
+                type="textarea"
+                :rows="item.rows || 3"
+                class="my-input__220"/>
+            </template>
+            <template v-if="item.type == 'switch'">
+              <el-switch
+                :value="getFormModel.form[item.field]"
+                :active-text="item.activeText"
+                :inactive-text="item.inactiveText"
+                :active-value="item.activeValue"
+                :inactive-value="item.inactiveValue">
+              </el-switch>
+            </template>
+            <template v-if="item.type === 'uploadImg'">
+              <el-upload
+                :action="uploadPath"
+                class="avatar-uploader"
+                :show-file-list="false"
+                name="multipartFile"
+                :data = "{}"
+                :on-success="e => {return hanleUploadSuccess(e, item)}"
+                >
+                  <img v-if="getFormModel.form[item.field]"
+                    :src="getFormModel.form[item.field]"
+                    style="width: 175px; height: 175px;"
+                    alt="uploader">
+                  <i v-else class="el-icon-plus avatar-uploader-icon" ></i>
+              </el-upload>
+            </template>
+          </el-form-item>
+      </el-form>
+      <div slot="footer">
+        <el-button @click="handleCancel" type="danger">cancel</el-button>
+        <el-button @click="handleSubmit" type="primary">submit</el-button>
+      </div>
     </el-dialog>
   </section>
 </template>
@@ -93,6 +94,8 @@
     },
     watch: {},
     methods: {
+      ...mapMutations(['DIALOG_CLAOSE_SYNC']),
+      ...mapActions(['DIALOG_CLOSE_ASYNC']),
       /**
        * [hanleUploadSuccess upload上传钩子]
        * @param  {[type]} res  [description]
@@ -106,6 +109,7 @@
        */
       handleCancel() {
         Toast({type: 'info', msg: '操作已取消!'})
+        this.DIALOG_CLOSE_ASYNC()
       },
       /**
        * [handleSubmit 提交事件]
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,19 @@ export const state = {
   dialogTitle: '提示'
 }
 
-export const mutations = {}
+export const mutations = {
+  DIALOG_CLAOSE_SYNC: (state) => {
+    state.isShowDialog = false
+    state.dialogTitle = '提示内容'
+  }
+}
 
-export const actions = {}
+export const actions = {
+  DIALOG_CLOSE_ASYNC: ({commit}) => {
+    setTimeout(() => {
+      commit('DIALOG_CLAOSE_SYNC')
+    }, 1000, false)
+  }
+}
 
 export const getters = {}
