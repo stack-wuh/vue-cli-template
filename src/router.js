83b54827862e05fb13143eca3cc7acf2dc008633
shadow <wuh131420@foxmail.com>
2019-03-28 15:42:55 +0800

    2019-03-28-[feat][添加错误页面和路由地址]

--- src/router.js
+++ src/router.js
@@ -46,5 +46,18 @@ export default new Router({
       path: '/signin',
       component: () => import ('@/views/signin')
     },
+    {
+      path: '*',
+      component: () => import('@/components/error/404')
+    },
+    {
+      path: '/403',
+      component: () => import('@/components/error/403')
+    },
+    {
+      path: '/500',
+      meta: {isRequest: true},
+      component: () => import('@/components/error/500')
+    }
   ]
 })
